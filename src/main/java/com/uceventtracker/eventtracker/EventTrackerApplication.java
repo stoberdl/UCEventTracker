082b1d86412b4f171ff5a2959e4fd24d4f5d6121
greeleywebdev <54598615+greeleywebdev@users.noreply.github.com>
2021-03-07 19:28:56 -0500

    luke - code review 1

--- src/main/java/com/uceventtracker/eventtracker/EventTrackerApplication.java
+++ src/main/java/com/uceventtracker/eventtracker/EventTrackerApplication.java
@@ -2,12 +2,25 @@ package com.uceventtracker.eventtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EventTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventTrackerApplication.class, args);
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**")
+                    .addResourceLocations("classpath:/styles/")
+                    .setCachePeriod(0);
+        }
     }
 
 }
