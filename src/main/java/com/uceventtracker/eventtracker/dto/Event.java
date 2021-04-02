5aa8bb27c38085e6acb5a9068763e2fabc3629c1
micahel williams <willi6mc@mail.uc.edu>
2021-04-02 19:01:06 -0400

    added sortByDate Test

--- src/main/java/com/uceventtracker/eventtracker/dto/Event.java
+++ src/main/java/com/uceventtracker/eventtracker/dto/Event.java
@@ -2,11 +2,14 @@ package com.uceventtracker.eventtracker.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 public @Data
 class Event {
     private String title;
     private String description;
     private String location;
+    private Date date;
     private String startTime;
     private String endTime;
     private String host;
--- src/test/java/com/uceventtracker/eventtracker/EventTrackerApplicationTests.java
+++ src/test/java/com/uceventtracker/eventtracker/EventTrackerApplicationTests.java
@@ -7,8 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +23,10 @@ class EventTrackerApplicationTests {
 
     @Autowired
     private IEventService eventService;
-    private Event event;
+
+    Event event1 = new Event();
+    Event event2 = new Event();
+    Event event3 = new Event();
 
     @Test
     void checkEventData(){
@@ -48,27 +52,42 @@ class EventTrackerApplicationTests {
     }
 
     private void givenEventData() {
-        Event event1 = new Event();
         event1.setTitle("UC Hackathon");
         event1.setLocation("Crosley tower");
+        try {
+            event1.setDate(new SimpleDateFormat("dd-MM-yyyy").parse("23-01-2021"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         event1.setStartTime("1pm");
         event1.setEndTime("5pm");
         event1.setDescription("Groups compete to solve problems");
         event1.setHost("CECH");
         eventDAO.save(event1);
 
-        Event event2 = new Event();
         event2.setTitle("Knitting");
         event2.setLocation("Wheelbarrow tower");
+        try {
+            event2.setDate(new SimpleDateFormat("dd-MM-yyyy").parse("6-01-2021"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         event2.setStartTime("3pm");
         event2.setEndTime("4pm");
         event2.setDescription("Groups compete to knit");
         event2.setHost("Knitting Club");
         eventDAO.save(event2);
 
-        Event event3 = new Event();
         event3.setTitle("Preventing Hacks");
         event3.setLocation("Mcdonalds");
+        try {
+            event3.setDate(new SimpleDateFormat("dd-MM-yyyy").parse("25-02-2021"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         event3.setStartTime("12pm");
         event3.setEndTime("2pm");
         event3.setDescription("Learn about ways to prevent hackers from accessing your data.");
@@ -98,5 +117,26 @@ class EventTrackerApplicationTests {
 
     private void thenDisplayListOfEvents() {
         assertEquals(3, allEvents.size());
+    }
+
+    @Test
+    void sortEventsByDate() {
+        givenEventData();
+        whenEventsFetched();
+        thenReturnEventsSortedByDate();
+    }
+
+    private void whenEventsFetched() {
+        allEvents = eventDAO.fetchAllEvents();
+    }
+
+    private void thenReturnEventsSortedByDate() {
+        List<Date> eventDates = new ArrayList();
+        for (Event e : allEvents) {
+            eventDates.add(e.getDate());
+        }
+        Collections.sort(eventDates);
+
+        assertEquals(event2.getDate(), eventDates.get(0));
     }
 }
