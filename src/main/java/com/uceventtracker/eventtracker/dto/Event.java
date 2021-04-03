87202ff8d0842ec784ee563f3e38f3829d70d029
micahel williams <willi6mc@mail.uc.edu>
2021-04-02 20:02:36 -0400

    Event is now a persistence object

--- src/main/java/com/uceventtracker/eventtracker/dto/Event.java
+++ src/main/java/com/uceventtracker/eventtracker/dto/Event.java
@@ -2,15 +2,34 @@ package com.uceventtracker.eventtracker.dto;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
+
+@Entity
+@Table(name = "EVENTS")
 public @Data
 class Event {
+
+    @Id
+    @Column(name = "title", nullable = false, unique = true, length = 512)
     private String title;
+
+    @Column(name = "description", nullable = false, length = 512)
     private String description;
+
+    @Column(name = "username", nullable = false, length = 512)
     private String location;
+
+    @Column(name = "date", nullable = false, length = 45)
     private Date date;
+
+    @Column(name = "startTime", nullable = false, length = 45)
     private String startTime;
+
+    @Column(name = "endTime", nullable = false, length = 45)
     private String endTime;
+
+    @Column(name = "host", nullable = false, length = 45)
     private String host;
 }
--- src/test/java/com/uceventtracker/eventtracker/EventTrackerApplicationTests.java
+++ src/test/java/com/uceventtracker/eventtracker/EventTrackerApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
