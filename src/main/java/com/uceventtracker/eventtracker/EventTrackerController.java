fbeab362d9c5eeca2b1ea5716aa4c9b83f01e077
PuranKansakar <phkansakar@gmail.com>
2021-04-04 16:37:53 -0400

    Search, save and create event in controller

--- src/main/java/com/uceventtracker/eventtracker/EventTrackerController.java
+++ src/main/java/com/uceventtracker/eventtracker/EventTrackerController.java
@@ -1,28 +1,86 @@
 package com.uceventtracker.eventtracker;
 
 import com.uceventtracker.eventtracker.dto.Event;
+import com.uceventtracker.eventtracker.service.EventService;
+import com.uceventtracker.eventtracker.service.IEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 public class EventTrackerController {
+
+    @Autowired
+    IEventService eventService;
+
     /**
      * Handle the / endpoint
      * @return start.html
      */
     @RequestMapping("/")
-    public String index(Model model) {
-        Event event = new Event();
-        event.setTitle("UC Hackathon");
-        event.setLocation("Crosley tower");
-        event.setStartTime("1pm");
-        event.setEndTime("5pm");
-        event.setDescription("Groups compete to solve problems");
-        event.setHost("CECH");
-        model.addAttribute(event);
+    public String index() {
         return "start";
     }
 
+    /**
+     * Handle the / endpoint
+     * @return createEvent.html
+     */
+    @RequestMapping("/createEvent")
+    public String createEvent(Model model) {
+        Event event = new Event();
+        event.setTitle("UC Hackathon");
+        event.setDescription("Groups compete to solve problems");
+        event.setLocation("Crosley tower");
+        event.setStartTime("1pm");
+        event.setEndTime("5pm");
+        event.setHost("CECH");
+        model.addAttribute(event);
+
+        return "createEvent";
+    }
+
+    @RequestMapping("/saveEvent")
+    public String saveEvent(Event event) {
+        try {
+            eventService.save(event);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "createEvent";
+        }
+
+        return "createEvent";
+    }
+
+    @PostMapping(value="/event", consumes="application/json", produces="application/json")
+    @ResponseBody
+    public Event createEvent(@RequestBody Event event) {
+        Event newEvent = null;
+        try {
+            newEvent = eventService.save(event);
+        } catch (Exception e) {
+            // TODO add logging
+        }
+        return newEvent;
+    }
+
+//    @GetMapping("/titles")
+//    public ResponseEntity searchTitles(@RequestParam(value="searchTerm", required=false, defaultValue="None") String searchTerm) {
+//        String newSearchTerm = searchTerm + "";
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
+
+    @GetMapping("/titles")
+    public ResponseEntity searchTitles(@RequestParam Map<String, String> requestParams) {
+        int params = requestParams.size();
+        String searchValue = requestParams.get("searchTerm");
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
